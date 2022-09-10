c28d6c5f56df6724a8024ebabf09cb88cad79178
janardhanRedde <badarijanardhanreddy912@gmail.com>
2022-09-10 17:52:38 +0530

    finalAssignement

---
+++ exercise_01/app.js
@@ -0,0 +1,10 @@
+
+const bodyEl = document.getElementsByTagName('body');
+
+
+function changeBackgroundColor(event) {
+    const backgroundColor = event?.target?.value;
+    bodyEl[0].style.backgroundColor = backgroundColor;
+}
+
+
